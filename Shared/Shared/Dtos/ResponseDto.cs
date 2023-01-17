1e43f9093d324783d9359c7ab72a0ec63d2d1b0f
salli <sezininyayla@gmail.com>
2023-01-17 09:56:13 +0300

    updated

--- Shared/Shared/Dtos/ResponseDto.cs
+++ Shared/Shared/Dtos/ResponseDto.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Text.Json.Serialization;
@@ -16,12 +17,37 @@ namespace Shared.Dtos
         [JsonIgnore]
         public bool IsSuccessful { get; set; }
         public List<string> Errors { get; set; }
-
+        //FactoryPattern Nesne uretme yolu statıc olarak
         public static ResponseDto<T> Success(T data,int statusCode)
         {
-
+            return new ResponseDto<T> { Data = data, StatusCode = statusCode, IsSuccessful=true };
         }
 
+        public static ResponseDto<T> Success(int statusCode)
+        {
+            return new ResponseDto<T> { Data = default(T), StatusCode = statusCode, IsSuccessful = true };
+        }
 
+        public static ResponseDto<T> Fail(List<string> errors, int statusCode)
+        {
+            return new ResponseDto<T>
+            {
+                Errors = errors,
+                StatusCode = statusCode,
+                IsSuccessful = false
+
+            };
+        }
+
+        public static ResponseDto<T> Fail(string error, int statusCode)
+        {
+            return new ResponseDto<T>
+            {
+                Errors = new List<string>() { error},
+                StatusCode = statusCode,
+                IsSuccessful = false
+
+            };
+        }
     }
 }
