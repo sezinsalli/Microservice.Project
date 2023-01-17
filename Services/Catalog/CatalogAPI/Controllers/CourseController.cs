b9226ab0ce296885ac1e11afcc51af8c465f5bd4
salli <sezininyayla@gmail.com>
2023-01-17 17:15:13 +0300

    CourseController

---
+++ Services/Catalog/CatalogAPI/Controllers/CourseController.cs
@@ -0,0 +1,30 @@
+﻿using CatalogAPI.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Shared.ControllerBases;
+using System.Threading.Tasks;
+
+namespace CatalogAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CourseController : CustomBaseController
+    {
+        private readonly ICourseService _courseService;
+        public CourseController(ICourseService courseService)
+        {
+            _courseService = courseService;
+        }
+        public async Task<IActionResult> GetAll()
+        {
+            var response= await _courseService.GetAllAsync();
+            return CreateActionResultInstance(response);
+        }
+        public async Task<IActionResult> GetById(string id)
+        {
+            var response=await _courseService.GetByIdAsync(id);
+            return CreateActionResultInstance(response);
+        }
+
+    }
+}
--- Services/Catalog/CatalogAPI/Services/ICourseService.cs
+++ Services/Catalog/CatalogAPI/Services/ICourseService.cs
@@ -7,6 +7,7 @@ namespace CatalogAPI.Services
 {
     public interface ICourseService
     {
+        Task<Response<List<CourseDto>>> GetAllAsync();
         Task<Response<CourseDto>> GetByIdAsync(string id);
         Task<Response<List<CourseDto>>> GetAllByUserIdAsync(string id);
         Task<Response<CourseDto>> CreateAsync(CourseCreateDto courseCreateDto);
