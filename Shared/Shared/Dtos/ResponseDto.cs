ad84f9b819a0ce1c37b7ba306693aae06522c692
salli <sezininyayla@gmail.com>
2023-01-17 09:31:27 +0300

    updated

---
+++ Shared/Shared/Dtos/ResponseDto.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace Shared.Dtos
+{
+    public class ResponseDto<T>
+    {
+        public T Data { get; set; }
+        
+        [JsonIgnore]
+        public int StatusCode { get; set; }
+        [JsonIgnore]
+        public bool IsSuccessful { get; set; }
+        public List<string> Errors { get; set; }
+
+        public static ResponseDto<T> Success(T data,int statusCode)
+        {
+
+        }
+
+
+    }
+}
