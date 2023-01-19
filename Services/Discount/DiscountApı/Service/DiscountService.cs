0bc52a9a8d155c7c29b54a7807f682ce8d7aba44
salli <sezininyayla@gmail.com>
2023-01-19 17:27:35 +0300

    updated

--- Services/Discount/DiscountApı/Service/DiscountService.cs
+++ Services/Discount/DiscountApı/Service/DiscountService.cs
@@ -1,20 +1,35 @@
-﻿using DiscountApı.Models;
+﻿using Dapper;
+using DiscountApı.Models;
+using Microsoft.Extensions.Configuration;
+using Npgsql;
 using Shared.Dtos;
 using System.Collections.Generic;
+using System.Data;
+using System.Linq;
 using System.Threading.Tasks;
 
 namespace DiscountApı.Service
 {
     public class DiscountService : IDiscountService
     {
+        private readonly IConfiguration _configuration;
+        private readonly IDbConnection _dbConnection;
+        public DiscountService(IConfiguration configuration, IDbConnection dbConnection)
+        {
+            _configuration = configuration;
+            _dbConnection = new NpgsqlConnection(_configuration.GetConnectionString("PostgreSql"));
+        }
+
         public Task<Response<NoContent>> Delete(int id)
         {
             throw new System.NotImplementedException();
         }
 
-        public Task<Response<List<Discount>>> GetAll()
+        public async Task<Response<List<Discount>>> GetAll()
         {
-            throw new System.NotImplementedException();
+            var discounts = await _dbConnection.QueryAsync<Models.Discount>("Select*from discount");
+
+            return Response<List<Discount>>.Success(discounts.ToList(), 200);
         }
 
         public Task<Response<Discount>> GetByCodeAndUserId(string code, string userId)
